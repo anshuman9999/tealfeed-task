372cd76df0c7e9f992c82ad4c40c4bb1b2195bce
anshuman9999 <anshuman.pandey9999@gmail.com>
2022-04-30 20:06:46 +0530

    change head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,20 @@ import type { NextPage } from "next";
 import Header from "components/Header";
 import CardWrapper from "components/CardWrapper";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const LayoutNoSSR = dynamic(() => import("components/Layout"), { ssr: false });
 
 const Home: NextPage = () => (
-  <LayoutNoSSR>
-    <Header />
-    <CardWrapper />
-  </LayoutNoSSR>
+  <>
+    <Head>
+      <title>Tealfeed Assignment | Anshuman Pandey</title>
+    </Head>
+    <LayoutNoSSR>
+      <Header />
+      <CardWrapper />
+    </LayoutNoSSR>
+  </>
 );
 
 export default Home;
--- pages/table.tsx
+++ pages/table.tsx
@@ -2,15 +2,21 @@ import type { NextPage } from "next";
 import Header from "components/Header";
 import Table from "components/Table";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const LayoutNoSSR = dynamic(() => import("components/Layout"), { ssr: false });
 
 const Home: NextPage = () => {
   return (
-    <LayoutNoSSR>
-      <Header />
-      <Table />
-    </LayoutNoSSR>
+    <>
+      <Head>
+        <title>Tealfeed Assignment | Anshuman Pandey</title>
+      </Head>
+      <LayoutNoSSR>
+        <Header />
+        <Table />
+      </LayoutNoSSR>
+    </>
   );
 };
 
